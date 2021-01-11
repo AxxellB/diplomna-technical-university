ca2029cec81b5bc389c4053402cfb1e66c11772b
AxxellB <axxellblaze@gmail.com>
2021-01-11 19:04:17 +0200

    Added environmental variables

--- FinalProjectForum/settings.py
+++ FinalProjectForum/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 from os.path import join
 from pathlib import Path
 
@@ -84,10 +85,11 @@ WSGI_APPLICATION = 'FinalProjectForum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DjangoForum',
-        'USER': 'postgres',
-        'PASSWORD': 'Zaek2009',
-        'PORT': '8080',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
